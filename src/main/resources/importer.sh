d567314a81d029e65cbb6a514772754abad5c235
Salvatore Impellizzeri <salvatore.impellizzeri@spindox.it>
2020-05-05 21:38:14 +0200

    fix importer.sh

--- src/main/resources/importer.sh
+++ src/main/resources/importer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-pkill -F importer.pid
 java -D'java.util.logging.config.file'=logging.properties -jar google-cloudsearch-sharepoint-connector-v1-0.0.5.jar &
 IMPORTER_PID=$!
 echo IMPORTER_PID
-echo IMPORTER_PID > importer.pid
+echo IMPORTER_PID > ../importer.pid
