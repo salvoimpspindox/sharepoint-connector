d8cfb777dffd212facb43f7dd64bfa86795628ec
Salvatore Impellizzeri <salvatore.impellizzeri@spindox.it>
2020-05-18 08:59:09 +0200

    fixed properties per production

--- src/main/resources/importer.sh
+++ src/main/resources/importer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -D'java.util.logging.config.file'=logging.properties -jar google-cloudsearch-sharepoint-connector-v1-0.0.5.jar &
+java -D'java.util.logging.config.file'=logging.properties -jar -Xmx3048m google-cloudsearch-sharepoint-connector-v1-0.0.5.jar &
 IMPORTER_PID=$!
 echo IMPORTER_PID
 echo IMPORTER_PID > ../importer.pid
